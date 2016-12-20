4fe197e80d89ee02f11fe9fbbd70a82ef04fdb5f
Luigi Calori <luigi.calori@gmail.com>
2016-12-20 01:29:59 +0100

    add recipes folder

---
+++ recipes/hosts/marconi/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ mkdir build_gnu
+ source /marconi/home/userinternal/lcalori0/spack/RCM_spack_deploy/deploy/spack/share/spack/setup-env.sh
+ module load cmake
+ module load git
+ module load libpthread-stubs
+ module load autoload openmpi
+
