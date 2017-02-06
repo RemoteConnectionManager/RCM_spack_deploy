8fe575a1fdc9f8d3154565925f8f4bdd0f942486
Luigi Calori <luigi.calori@gmail.com>
2017-02-06 20:41:38 +0100

    remove merged pr and added jmol pr

--- deploy_integrate.sh
+++ deploy_integrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python scripts/deploy_setup.py --integration --dest=deploy/spack --debug=debug  --branches clean/develop     pr/.*  wip/.*  --prlist 2763 2771 2902 2686 2928
+python scripts/deploy_setup.py --integration --dest=deploy/spack --debug=debug  --branches clean/develop     pr/.*  wip/.*  --prlist 2686 2771 2902 3041
