f68705a39db303a4c2ed110b5e6f949e17c98a41
Luigi Calori <luigi.calori@gmail.com>
2017-02-08 10:35:12 +0100

    update to new pr

--- deploy_integrate.sh
+++ deploy_integrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python scripts/deploy_setup.py --integration --dest=deploy/spack --debug=debug  --branches clean/develop     pr/.*  wip/.*  --prlist 2686 2771 2902 3041
+python scripts/deploy_setup.py --integration --dest=deploy/spack --debug=debug  --branches clean/develop     pr/.*  wip/.*  --prlist 2686 2771 2902 3041 3057
