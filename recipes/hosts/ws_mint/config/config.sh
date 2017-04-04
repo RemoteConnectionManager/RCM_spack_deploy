68925e9e457eadfe95a1d2114414fc79191f36f0
Luigi Calori <luigi.calori@gmail.com>
2017-04-04 17:00:35 +0200

    set build recipe for client on mint

--- recipes/hosts/ws_mint/config/config.sh
+++ recipes/hosts/ws_mint/config/config.sh
@@ -2,8 +2,9 @@
 spack repo add @{RCM_DEPLOY_ROOTPATH}/repo --scope site
 spack  compiler find --scope site
 #spack info rcm
-spack install --verbose py-flake8
-spack install --verbose environment-modules~X
+spack install py-flake8
+spack install environment-modules~X
+spack install --only dependencies rcm+client -server
 #spack find
 #spack module  rm -y
 spack module refresh -y
