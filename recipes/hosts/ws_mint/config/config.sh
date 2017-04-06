cf2d90838d2049c19573264284f8b09aea458fc3
Luigi Calori <luigi.calori@gmail.com>
2017-04-06 14:41:28 +0200

    record build history

--- recipes/hosts/ws_mint/config/config.sh
+++ recipes/hosts/ws_mint/config/config.sh
@@ -4,7 +4,8 @@ spack  compiler find --scope site
 #spack info rcm
 spack install environment-modules~X
 spack install py-flake8
-spack install --only dependencies rcm+client -server
+spack install --only dependencies rcm@develop+client +linksource -server
 #spack find
 #spack module  rm -y
 spack module refresh -y
+#spack diy --source-path /kubuntu/home/lcalori/spack/RCM rcm@develop+client +linksource -server
