b7c27b8dd08ff3187b661d66c168e8faf2b3b9d2
Luigi Calori <luigi.calori@gmail.com>
2017-04-06 14:15:09 +0200

    add flake8

--- recipes/hosts/ws_mint/config/config.sh
+++ recipes/hosts/ws_mint/config/config.sh
@@ -2,8 +2,8 @@
 spack repo add @{RCM_DEPLOY_ROOTPATH}/repo --scope site
 spack  compiler find --scope site
 #spack info rcm
-spack install py-flake8
 spack install environment-modules~X
+spack install py-flake8
 spack install --only dependencies rcm+client -server
 #spack find
 #spack module  rm -y
