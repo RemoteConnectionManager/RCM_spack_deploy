9f365bdc5324244f0f5e8b1a069ab6b440126105
Elda Rossi <propro01@r000u08l03.marconi.cineca.it>
2017-11-27 20:26:41 +0100

    fix marconi production deployment

--- recipes/hosts/marconi/setup.sh
+++ recipes/hosts/marconi/setup.sh
@@ -12,18 +12,18 @@ while [ -h "$SOURCE" ]; do
 done
 export RCM_DEPLOY_HOSTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 source ${RCM_DEPLOY_HOSTPATH}/../../../get_root
-export CONFIG_DIRS="config config/shared_install config/rcm config/rcm/server config/rcm/server/headless"
+export CONFIG_DIRS="config config/shared_install config/rcm config/rcm/server config/rcm/server/headless config/insitu "
 echo "-->$CONFIG_DIRS<--"
 if [ "x$1" != "x" ]
 then
   RCM_DEPLOY_CURRENT_PATH=deploy/insitu/$1/spack
   if [ "$1" == "dev" ]
   then
-    export CONFIG_DIRS="$CONFIG_DIRS config/insitu" 
+    export CONFIG_DIRS="$CONFIG_DIRS config/rcm/develop" 
   fi  
   if [ "$1" == "dev_all" ]
   then
-    export CONFIG_DIRS="$CONFIG_DIRS config/openmpi_scheduler config/insitu config/meteo"
+    export CONFIG_DIRS="$CONFIG_DIRS config/openmpi_scheduler config/openfoam config/meteo config/rcm/develop"
   fi  
 
 python ${RCM_DEPLOY_ROOTPATH}/scripts/config.py -c $CONFIG_DIRS  --platformconfig --runconfig --dest ${RCM_DEPLOY_CURRENT_PATH}
