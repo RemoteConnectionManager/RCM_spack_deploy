16cf14a7cf74e1385cf0c85cd4913828b941d585
Luigi Calori <luigi.calori@gmail.com>
2018-03-05 12:15:31 +0100

    add heavvy install to host config.sh

--- recipes/hosts/galileo/config/config.sh
+++ recipes/hosts/galileo/config/config.sh
@@ -4,4 +4,10 @@ spack find -l curl
 spack spec -l curl
 spack install curl
 spack module refresh -y
-moule load curl
+module load curl
+spack  install --only dependencies rcm@develop
+spack  install py-flake8
+spack  install git
+spack  install paraview
+spack  install rstudio
+
