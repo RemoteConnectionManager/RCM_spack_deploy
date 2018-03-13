2b91bf84ae035726f1d1fd888da8d2f7a4bc827d
Luigi Calori <luigi.calori@gmail.com>
2018-03-13 18:09:50 +0100

    galileo temporary production

---
+++ recipes/hosts/galileo/README.md
@@ -0,0 +1,12 @@
+# Deploy status on  hpc3
+
+First step: clone this repo:
+
+    git clone -b dev  https://github.com/RemoteConnectionManager/RCM_spack_deploy.git <folder name>
+    cd <folder name>
+
+link here a pre-filled cache of all downloaded artifact
+
+    ln -s /hpc/home/cibo19/spack/cache .
+    source recipes/hosts/eni/setup.sh prod
+
--- recipes/hosts/galileo/setup.sh
+++ recipes/hosts/galileo/setup.sh
@@ -12,22 +12,26 @@ while [ -h "$SOURCE" ]; do
 done
 export RCM_DEPLOY_HOSTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 source ${RCM_DEPLOY_HOSTPATH}/../../../get_root
-export CONFIG_DIRS="config config/shared_install config/rcm config/rcm/server config/rcm/server/gpu config/insitu "
+export CONFIG_DIRS="config config/shared_install config/rcm config/rcm/server config/rcm/server/gpu config/insitu config/meteo config/rstudio "
 echo "-->$CONFIG_DIRS<--"
 if [ "x$1" != "x" ]
 then
-  RCM_DEPLOY_CURRENT_PATH=deploy/insitu/$1/spack
+  RCM_DEPLOY_CURRENT_PATH=deploy/$(hostname -f | cut -f2 -d'.')/$1/spack
   if [ "$1" == "dev" ]
   then
     export CONFIG_DIRS="$CONFIG_DIRS config/rcm/develop" 
   fi  
+  if [ "$1" == "prod" ]
+  then
+    export CONFIG_DIRS="$CONFIG_DIRS config/rcm/production"
+  fi
   if [ "$1" == "dev_all_mpi" ]
   then
-    export CONFIG_DIRS="$CONFIG_DIRS config/openmpi_scheduler config/openfoam config/meteo config/rstudio config/rcm/develop"
+    export CONFIG_DIRS="$CONFIG_DIRS config/openmpi_scheduler config/openfoam config/rcm/develop"
   fi  
-  if [ "$1" == "dev_all3" ]
+  if [ "$1" == "dev_all" ]
   then
-    export CONFIG_DIRS="$CONFIG_DIRS config/openfoam config/meteo config/rstudio config/rcm/develop"
+    export CONFIG_DIRS="$CONFIG_DIRS config/openfoam config/rcm/develop"
   fi  
 
 python ${RCM_DEPLOY_ROOTPATH}/scripts/config.py -c $CONFIG_DIRS  --platformconfig --runconfig --dest ${RCM_DEPLOY_CURRENT_PATH}
