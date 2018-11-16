627a3607ba87de7e3191ae370043e771087537b4
Luigi Calori <luigi.calori@gmail.com>
2018-11-16 16:26:57 +0100

    make pyyaml import compatible with python3

--- scripts/utils/external/__init__.py
+++ scripts/utils/external/__init__.py
@@ -3,6 +3,10 @@ import os,sys
 
 basepath = os.path.dirname(os.path.realpath(__file__))
 sys.path.insert(0, basepath)
+if sys.version_info.major == 3 :
+    sys.path.insert(0, os.path.join(basepath,'PyYAML-3.13','lib3'))
+else:
+    sys.path.insert(0, os.path.join(basepath,'PyYAML-3.13','lib'))
 
 
 #from . import hiyapyco,yaml
