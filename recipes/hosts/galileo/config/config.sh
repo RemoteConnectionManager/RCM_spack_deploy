03b24aaa9437449a98fc3c785bba07a2e91cb862
Luigi Calori <a06ccc09@node131.galileo.cineca.it>
2019-07-23 18:11:53 +0200

    add pre-installed galileo compilers, tested with openfoam and opendoam@intel

--- recipes/hosts/galileo/config/config.sh
+++ recipes/hosts/galileo/config/config.sh
@@ -1,5 +1,5 @@
 spack mirror  add --scope site  common_cache file:///gpfs/work/DATA/spack/cache
-spack install -v gcc@7.3.0
- spack module tcl refresh -y gcc@7.3.0
-module load gcc; spack  compiler find --scope site
-spack install -v openmpi
+#spack install -v gcc@7.3.0
+# spack module tcl refresh -y gcc@7.3.0
+#module load gcc; spack  compiler find --scope site
+#spack install -v openmpi
