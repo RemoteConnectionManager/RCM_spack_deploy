47d5074b291abf03f639930f35ed9b7f5985c4b1
Luigi Calori <luigi.calori@gmail.com>
2019-09-06 01:16:41 +0200

    build on Galileo

--- recipes/hosts/galileo/config/config.sh
+++ recipes/hosts/galileo/config/config.sh
@@ -1,4 +1,4 @@
-spack mirror  add --scope site  common_cache file:///gpfs/work/DATA/spack/cache
+#spack mirror  add --scope site  common_cache file:///gpfs/work/DATA/spack/cache
 #spack install -v gcc@7.3.0
 # spack module tcl refresh -y gcc@7.3.0
 #module load gcc; spack  compiler find --scope site
